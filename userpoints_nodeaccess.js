5b604a552da55041b929f4eba4d7f9ea66294a17
Sascha Grossenbacher <saschagros@gmail.com>
2010-09-03 12:44:29 +0200

    fixing bugs

--- userpoints_nodeaccess.js
+++ userpoints_nodeaccess.js
@@ -1,19 +1,38 @@
 // $Id$
-
 (function ($) {
-
   Drupal.behaviors.userpointsNodeAccessFieldsetSummaries = {
     attach: function (context) {
       $('fieldset.userpoints-nodeaccess-form', context).drupalSetSummary(function (context) {
-        if ($('#edit-points-price', context).val() > 0) {
+        if ($('#edit-userpoints-nodeaccess-points-price', context).val() > 0) {
           var params = {
-            '@points': $('#edit-points-price', context).val(),
-            '@category': $('select#edit-points-category :selected', context).text()
+            '@points': $('#edit-userpoints-nodeaccess-points-price', context).val(),
+            '@category': $('select#edit-userpoints-nodeaccess-points-category :selected', context).text()
           };
           return Drupal.t('@points in category @category required.', params);
         }
         else {
           return Drupal.t('No points required.');
+        }
+      });
+    }
+  };
+  Drupal.behaviors.userpointsNodeAccessNodeTypeFieldsetSummaries = {
+    attach: function (context) {
+      $('fieldset.userpoints-nodeaccess-nodetype-form', context).drupalSetSummary(function (context) {
+        if ($('#edit-userpoints-nodeaccess-enabled').is(':checked')) {
+          if ($('#edit-userpoints-nodeaccess-points-price', context).val() > 0) {
+            var params = {
+              '@points': $('#edit-userpoints-nodeaccess-points-price', context).val(),
+              '@category': $('select#edit-userpoints-nodeaccess-points-category :selected', context).text()
+            };
+            return Drupal.t('Userpoints node access enabled. @points in category @category required.', params);
+          }
+          else {
+            return Drupal.t('Userpoints node access enabled. No points required by default.');
+          }
+        }
+        else {
+          return Drupal.t('Userpoints node access not enabled.');
         }
       });
     }
